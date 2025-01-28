dd12bd5813c651c4c3a1397b466892f6585ab9b9
Alex Birdsall <ambirdsall@gmail.com>
2025-01-28 15:32:24 -0800

    clean up temporarily-needed assets

--- scripts/postrender.sh
+++ scripts/postrender.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 
+# copy over non-compiled assets
 cp src/pages/index.css dist/
 cp src/images/favicon-32x32.png dist/
 cp -r src/images/icons dist/
+
+# remove compiled js only needed for one-time render
+rm -r dist/scripts
+rm -r dist/src
