71146ac841d4e40578caf4091e005e3ba67b8065
Alex Birdsall <ambirdsall@gmail.com>
2025-01-28 15:22:04 -0800

    Prefer sh redirection to fs.write
    Really, just deleting a reference comment.

--- scripts/render.js
+++ scripts/render.js
@@ -3,8 +3,8 @@ import ReactDOMServer from "react-dom/server"
 import Page from "../src/pages/index.js"
 import fs from "fs"
 
+// const html = ReactDOMServer.renderToStaticMarkup(<Page sha='butts.gov' />)
 const html = ReactDOMServer.renderToStaticMarkup(<Page />)
 const fullHtml = `<!DOCTYPE html>${html}`
 
 console.log(fullHtml)
-// fs.writeFileSync("dist/index.html", fullHtml)
